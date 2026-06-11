1f1e0d611cd871f11093ac730d112e1d66e843c3
twotwoship <yangbae@DESKTOP-7CFQ9ND.localdomain>
2026-06-11 20:51:53 +0900

    led_circulation

---
+++ Embedded/atmega128/button/lesson_4(LED_circulation)/button.h
@@ -0,0 +1,27 @@
+﻿/*
+ * button.h
+ *
+ * Created: 2026-06-10 오전 11:45:01
+ *  Author: user
+ */ 
+
+#define F_CPU 16000000UL  //16MHz
+#include <avr/io.h>  // PORTA PORTB PORTD... IO관련 reg가 들어 있다. 
+#include <util/delay.h>  // _delay_ms _delay_us 등 
+
+#define  BUTTON_DDR  DDRD
+#define  BUTTON_PIN  PIND   // PORTD를 읽는 register 5v:1  0v:0 
+
+#define  BUTTON0PIN 3    // PORTD.4
+#define  BUTTON1PIN 4    // PORTD.5
+#define  BUTTON2PIN 5    // PORTD.6
+#define  BUTTON3PIN 6    // PORTD.7
+
+#define  BUTTON0   0   // PORTD.3의 가상 index (sw번호)
+#define  BUTTON1   1   // PORTD.4의 가상 index (sw번호)
+#define  BUTTON2   2   // PORTD.5의 가상 index (sw번호)
+#define  BUTTON3   3   // PORTD.6의 가상 index (sw번호)
+#define  BUTTON_NUMBER  4   // 버튼 갯수 
+
+#define  BUTTON_PRESS  1      // 버튼을 누르면 high (active-high)
+#define  BUTTON_RELEASE 0     // 버튼을 뗀 상태 (low)
---
+++ Embedded/atmega128/button/lesson_4(LED_circulation)/main.c
@@ -0,0 +1,80 @@
+﻿/*
+ * 01.LED_CONTROL.c
+ *
+ * Created: 2026-06-10 오전 10:20:27
+ * Author : user
+ */ 
+
+#define F_CPU 16000000UL   // 16MHz
+#include <avr/io.h>  // PORTA PORTD 등의 I/O register들이 들어 있다. 
+#include <util/delay.h>   // _delay_ms _delay_us 등의 함수 가 들어 있다. 
+
+#include "button.h"
+#include "led.h"
+
+extern void init_led(void);  // init_led함수는 다른 화일에 들어 있다고 compiler 신고 
+extern void init_button(void);
+extern int get_button(int button_num, int button_pin);
+extern void led_all_on(void);
+extern void led_all_off(void);
+extern int led_main(void);
+extern void led_shift_left_on(void);
+extern void led_shift_right_on(void);
+
+
+#if 1
+int main(void)
+{
+	void (*fp[8])(void) = {
+		led_all_off, led_all_on, led_shift_left_on, led_shift_right_on,
+		led_shift_left_keepon, led_shift_right_keepon, led_flower_on,
+		led_flower_off
+	};
+	
+	int button0_state = 1;   // 초기 상태를 led all off로 출발 하자 
+	int check = 0;
+	
+	init_button();
+	init_led();
+	led_all_off();
+	// led_main();
+	while (1)
+	{
+#if 1
+		// button0_state : 0 --> led_shift_left_on
+		// button0_state : 1 --> led_shift_right_on
+		fp[check]();
+		if (get_button(BUTTON0, BUTTON0PIN))
+		{	
+			check = (check + 1) % 8;
+			led_all_off();
+		}
+#else		
+		// toggle  off <--> on
+		if (get_button(BUTTON0, BUTTON0PIN))
+		{
+			button0_state = !button0_state;   // 반전  0 <--> 1
+			if (button0_state)  
+				led_all_on();
+			else led_all_off();
+		}
+#endif
+	}
+}
+#endif
+
+#if 0
+int main(void)
+{
+	DDRA = 0b11111111;    // PORTA에 LED가 8개 연결 되어 있으므로 all 1(출력)으로 설정 한다.
+	                      // DDR(Data Direction Register) 방향 설정
+						  // 1: 출력 0:입력 
+    while (1) 
+    {
+		PORTA = 0b11111111;   // all on
+		_delay_ms(1000);   // 1초 유지  1000ms : 1초 500ms : 0.5초
+		PORTA = 0b00000000;  // all off
+		_delay_ms(1000);    // 1초 sleep
+    }
+}
+#endif 
