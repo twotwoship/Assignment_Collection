ac2ffe6f940ce0dbf609b7e29f54582049315532
twotwoship <yangbae@DESKTOP-7CFQ9ND.localdomain>
2026-06-25 10:53:30 +0900

    servo piezo buzzer

---
+++ Embedded/atmega128/pwm/_servo_piezo_buzzer/main.c
@@ -0,0 +1,20 @@
+/*
+ * 08.pwm_servo_piezo_buzzer.c
+ *
+ * Created: 2026-06-25 오전 9:46:13
+ * Author : kccistc
+ */ 
+
+#include <avr/io.h>
+#include "pwm.h"
+
+
+int main(void)
+{
+	servo_motor_main();
+
+    while (1) 
+    {
+    }
+}
+
---
+++ Embedded/atmega128/pwm/_servo_piezo_buzzer/pwm.h
@@ -0,0 +1,19 @@
+﻿/*
+ * pwm.h
+ *
+ * Created: 2026-06-18 오후 2:22:46
+ *  Author: kccistc
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include "button.h"
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+void init_timer1_pwm(void);
+int servo_motor_main(void);
+
+#endif /* PWM_H_ */
